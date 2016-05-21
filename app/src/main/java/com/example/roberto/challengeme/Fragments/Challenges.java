f44a0453ae52d0d61a98fe4db497425df5015633
Joshua Cantero <cante008@cougars.csusm.edu>
2016-05-21 15:40:14 -0700

    Changes to the Challenge Class
    Connecting the widgets, from xml to Java.

--- app/src/main/java/com/example/roberto/challengeme/Fragments/Challenges.java
+++ app/src/main/java/com/example/roberto/challengeme/Fragments/Challenges.java
@@ -1,11 +1,46 @@
 package com.example.roberto.challengeme.Fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.roberto.challengeme.R;
 
 /**
  * Created by Roberto on 5/8/2016.
  */
 public class Challenges extends Fragment {
+
+    TextView challenge; // Declaration for challenge text
+    TextView userName;
+    ImageView userImage;
+
+    @Override
+    public void onCreate(Bundle savedInstance)
+    {
+        super.onCreate(savedInstance);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_challenges, parent,true);
+        TextView challenge = (TextView) view.findViewById(R.id.tvChallenge);
+        TextView userName = (TextView) view.findViewById(R.id.tvUserName);
+        ImageView userImage = (ImageView) view.findViewById(R.id.ivUserImage);
+
+        
+
+
+        return view;
+    }
+
+
 }
 
 
