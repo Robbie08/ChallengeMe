2c53e0801a094f9aa0bc5678a27cdd31a821525f
Joshua Cantero <cante008@cougars.csusm.edu>
2016-05-21 15:01:10 -0700

    Refactor

--- app/src/main/java/com/example/roberto/challengeme/MainActivity.java
+++ app/src/main/java/com/example/roberto/challengeme/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        pagerTab();
 
     }
 
